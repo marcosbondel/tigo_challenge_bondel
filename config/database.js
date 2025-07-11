520c5a604d96822bab4d7773f4fef7bcd09f1cdd
Marcos Bonifasi <marcosbonifasi19@gmail.com>
2025-07-10 19:49:04 -0600

    feat: handlers for mongodb

--- config/database.js
+++ config/database.js
@@ -27,7 +27,7 @@ SOFTWARE.
 */
 
 const mongoose = require('mongoose')
-const { MongoClient } = require('mongodb')
+const { MongoClient, ObjectId } = require('mongodb')
 let mongo_client_instance = null
 const uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/bondel_challenge'
 
@@ -70,7 +70,7 @@ const create_collection = async (collection_name) => {
         await mongodb_connection()
     }
 
-    const db = mongo_client_instance.db(process.env.MONGODB_DB_NAME || 'bondel_challenge_development')
+    const db = mongo_client_instance.db(process.env.MONGODB_DB_NAME)
     const collections = await db.listCollections({ name: collection_name }).toArray()
 
     if (collections.length === 0) {
@@ -89,7 +89,7 @@ const create_collection_document = async (collection_name, document) => {
             mongo_client_instance = await mongodb_connection()
         }
 
-        const dbName = process.env.MONGODB_DB_NAME || 'bondel_challenge_development'
+        const dbName = process.env.MONGODB_DB_NAME
         const db = mongo_client_instance.db(dbName)
 
         if (!collection_name || typeof collection_name !== 'string') {
@@ -107,8 +107,8 @@ const create_collection_document = async (collection_name, document) => {
         // return result
         return {
             success: true,
-            message: `Document inserted into ${collection_name}`,
-            insertedId: result.insertedId
+            message: `Document inserted into ${collection_name} with id: ${result.insertedId}`,
+            data: result
         }
     } catch (error) {
         console.error(`Error inserting document into "${collection_name}":`, error.message)
@@ -120,7 +120,147 @@ const create_collection_document = async (collection_name, document) => {
     }
 }
 
+const update_collection_document = async (collection_name, documentId, updateParams) => {
+    try {
+        // Asegura que haya una conexión activa
+        if (!mongo_client_instance) {
+            mongo_client_instance = await mongodb_connection()
+        }
+
+        const dbName = process.env.MONGODB_DB_NAME
+        const db = mongo_client_instance.db(dbName)
+        const collection = db.collection(collection_name)
+
+        // Validación: documentId debe ser ObjectId válido
+        if (!ObjectId.isValid(documentId)) {
+            return {
+                success: false,
+                message: `Invalid ObjectId: ${documentId}`
+            }
+        }
+
+        const result = await collection.updateOne(
+            { _id: new ObjectId(documentId) },
+            { $set: updateParams }
+        )
+
+        if (result.matchedCount === 0) {
+            return {
+                success: false,
+                message: `No document found in "${collection_name}" with id: ${documentId}`
+            }
+        }
+
+        return {
+            success: true,
+            message: `Document in "${collection_name}" with id: ${documentId} updated successfully`
+        }
+
+    } catch (err) {
+        console.error(`[update_collection_document] Error:`, err)
+        return {
+            success: false,
+            message: `Error updating document: ${err.message}`,
+            error: err
+        }
+    }
+}
+
+const find_document_by_params = async(collection_name, params) => {
+    if (!mongo_client_instance) {
+        mongo_client_instance = await mongodb_connection()
+    }
+
+    const db = mongo_client_instance.db(process.env.MONGODB_DB_NAME)
+    let collection = db.collection(collection_name) // Replace with your collection name
+    let document = await collection.findOne(params)
+    
+    if (!document) {
+        console.log(`No document found in ${collection_name} with params:`, params)
+        return { 
+            success: false, 
+            message: `No document found in ${collection_name} with params: ${JSON.stringify(params)}`,
+        }
+    } else {
+        console.log(`Document(s) found in ${collection_name} with params:`, params)
+        return { success: true, data: document }
+    }
+
+}
+
+
+const list_collection_documents = async (collection_name) => {
+    if (!mongo_client_instance) {
+        mongo_client_instance = await mongodb_connection()
+    }
+
+    try {
+        const db = mongo_client_instance.db(process.env.MONGODB_DB_NAME)
+        const collections = await db.listCollections({ name: collection_name }).toArray()
+        
+        if (collections.length === 0) {
+            console.log(`Collection "${collection_name}" does not exist`)
+            return { success: false, message: `Collection "${collection_name}" does not exist` }
+        }
+
+        const collection = db.collection(collection_name)
+
+        const documents = await collection.find({}).toArray()
+
+        return { success: true, data: documents }
+    } catch (error) {
+        console.error(`Error listing documents in "${collection_name}":`, error)
+        return { success: false, error: error.message }
+    }
+}
+
+const delete_collection_document = async (collection_name, documentId) => {
+    if (!mongo_client_instance) {
+        mongo_client_instance = await mongodb_connection()
+    }
+
+    const db = mongo_client_instance.db(process.env.MONGODB_DB_NAME)
+    const collection = db.collection(collection_name)
+
+    try {
+        const result = await collection.deleteOne({ _id: new ObjectId(documentId) })
+
+        if (result.deletedCount === 0) {
+            return { success: false, message: `No document found with id: ${documentId}` }
+        }
+
+        return { success: true, message: `Document with id: ${documentId} deleted successfully` }
+    } catch (error) {
+        console.error(`Error deleting document from "${collection_name}":`, error)
+        return { success: false, error: error.message }
+    }
+}
+
+const delete_collection = async (collection_name) => {
+    if (!mongo_client_instance) {
+        mongo_client_instance = await mongodb_connection()
+    }
+
+    const db = mongo_client_instance.db(process.env.MONGODB_DB_NAME)
+    const collections = await db.listCollections({ name: collection_name }).toArray()
+
+    if (collections.length === 0) {
+        console.log(`Collection ${collection_name} does not exist`)
+        return { success: false, message: `Collection ${collection_name} does not exist` }
+    } else {
+        await db.dropCollection(collection_name)
+        console.log(`Collection ${collection_name} deleted`)
+        return { success: true, message: `Collection ${collection_name} deleted` }
+    }
+}
+
 module.exports = {
     mongoose_connection,
-    create_collection
+    create_collection,
+    create_collection_document,
+    find_document_by_params,
+    update_collection_document,
+    list_collection_documents,
+    delete_collection_document,
+    delete_collection
 }
